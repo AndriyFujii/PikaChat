6960db6310f64533f7b435ba2e334645092b1847
Andriy Fujii Pendloski <afp@edu.univali.br>
2021-11-06 23:24:57 -0300

    add UI asking for username and group name

--- PikaChat.py
+++ PikaChat.py
@@ -1,12 +1,7 @@
 import pika, os, threading
 import time, json, datetime
 from dateutil import parser
-
-json_data = {}
-user_name = "Andriy"
-queue_name = "Andriy"
-exchange_name = "sisdis"
-stopConsuming = False
+import PySimpleGUI as sg
 
 # Read the key from a txt
 file = open('amqpKey.txt', 'r')
@@ -30,8 +25,8 @@ def consume():
     connectionConsumer.call_later(1, callback)  
     channelConsumer = connectionConsumer.channel() # start a channel
     channelConsumer.exchange_declare(exchange=exchange_name, exchange_type='fanout')
-    channelConsumer.queue_declare(queue=queue_name) # Declare a queue
-    channelConsumer.queue_bind(queue=queue_name, exchange=exchange_name)
+    channelConsumer.queue_declare(queue=user_name) # Declare a queue
+    channelConsumer.queue_bind(queue=user_name, exchange=exchange_name)
     
     def on_message(channel, method, properties, body):
         #print ('Messages in queue now %d' % res.method.message_count)
@@ -44,7 +39,7 @@ def consume():
         print("[" + str(date) + "] <" + data['user'] + "> " + data['message'])
         
     
-    channelConsumer.basic_consume(queue_name, on_message, auto_ack=True)
+    channelConsumer.basic_consume(user_name, on_message, auto_ack=True)
     
     channelConsumer.start_consuming()
 
@@ -64,9 +59,9 @@ def publish():
                 json_data["timestamp"] = str(datetime.datetime.now().replace(microsecond=0).isoformat())
                 json_data["message"] = message
                 
+                #channelPublisher.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(json_data))
                 channelPublisher.basic_publish(exchange=exchange_name, routing_key='', body=json.dumps(json_data))
-                #channelPublisher.basic_publish(exchange=exchange_name, routing_key='', body=message)
-                #channelPublisher.basic_publish(exchange=exchange_name, properties=properties , routing_key='', body=message)
+
             except EOFError:
                 stopConsuming = True
                 print("Ending the program")
@@ -74,6 +69,40 @@ def publish():
         except KeyboardInterrupt:
             print("Closing")
 
+
+json_data = {}
+user_name = ""
+queue_name = ""
+exchange_name = ""
+isGroupChat = 1
+stopConsuming = False
+
+layout = [
+            [sg.Text('Username', size=(8)), sg.In(key='USER')],
+            [sg.Text('Group', size=(8), key='T1'), sg.In(key='GROUP')],
+            [sg.Button('Ok'), sg.Button('Exit')]
+         ]
+
+window = sg.Window('PikaChat', layout)
+
+while True:             # Event Loop
+    event, values = window.read()
+    
+    if event in (sg.WINDOW_CLOSED, 'Exit'):
+        break
+    elif event == 'Ok':
+        user_name = values['USER']
+        text = values['GROUP']
+        
+        if user_name == '':
+            sg.popup_error("Username can't be empty!")
+        elif text == '':
+            sg.popup_error("Group name can't be empty!")
+        else:
+            exchange_name = text
+            break
+        
+window.close()
 
 consumerThread = threading.Thread(target=consume)
 publisherThread = threading.Thread(target=publish)
