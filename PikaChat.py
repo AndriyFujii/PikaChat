5f6d8adfb02b0d6be54f2ae7df8adaaec180433b
Andriy Fujii Pendloski <afp@edu.univali.br>
2021-11-07 01:09:39 -0300

    Publish to exchange with UI

--- PikaChat.py
+++ PikaChat.py
@@ -3,8 +3,7 @@ import time, json, datetime
 from dateutil import parser
 import PySimpleGUI as sg
 
-text = ""
-stopConsuming = False
+
 
 # Read the key from a txt
 file = open('key.txt', 'r')
@@ -31,51 +30,26 @@ def consume(outBox):
     
     def on_message(channel, method, properties, body):
         global text
-        #print ('Messages in queue now %d' % res.method.message_count)
-        #print("\nMessage: ")
-        #print("\t%r: " % method)
-        #print("\t%r: " % properties)
-        
         data = json.loads(body)
         date = parser.parse(data['timestamp'])
-        text += "[" + str(date) + "] <" + data['user'] + "> " + data['message'] + '\n'
+        text += "[" + str(date) + "] <" + data['user'] + "> " + data['message']# + '\n'
         
+        # Multiline is disabled to prevent writing in it
         outBox.update(disabled = False)
         outBox.update(text)
         outBox.update(disabled = True)
-        #print(text)
         
     
     channelConsumer.basic_consume(user_name, on_message, auto_ack=True)
     
     channelConsumer.start_consuming()
 
-def publish(message):
-    #global stopConsuming
-    
-    connectionPublisher = pika.BlockingConnection(params)
-    channelPublisher = connectionPublisher.channel() # start a channel
-    channelPublisher.exchange_declare(exchange=exchange_name, exchange_type='fanout')
-
-    #while not stopConsuming:
-    #try:
-        #try:
-    message = input("Type anything to send a message: ")
-    
-    json_data["user"] = user_name
-    json_data["timestamp"] = str(datetime.datetime.now().replace(microsecond=0).isoformat())
-    json_data["message"] = message
-    
-    #channelPublisher.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(json_data))
-    channelPublisher.basic_publish(exchange=exchange_name, routing_key='', body=json.dumps(json_data))
-
-
 json_data = {}
 user_name = ""
 queue_name = ""
 exchange_name = ""
-
-#stopConsuming = False
+close_program = False
+stopConsuming = False
 
 # Username and group name UI
 layout = [
@@ -89,6 +63,7 @@ while True:
     event, values = window.read()
     
     if event in (sg.WINDOW_CLOSED, 'Exit'):
+        close_program = True
         break
     elif event == 'Ok':
         user_name = values['USER']
@@ -109,11 +84,12 @@ window.close()
 def collapse(layout, key, visible):
     return sg.pin(sg.Column(layout, key=key, visible=visible, pad=(0,0)))
 
-# Chat box UI
+# Section that's gonna be hidden
 section = [
            [sg.Text('User', size=(8)), sg.In(key='USER'),
             sg.Button('Set')]
           ]
+# Chat UI
 layout = [
             [sg.Radio('Group chat', "RADIO1", default=True, enable_events=True, key='R1'),
              sg.Radio('Private chat', "RADIO1", enable_events=True, key='R2'),
@@ -123,45 +99,47 @@ layout = [
              sg.Button('Send', visible=False, bind_return_key=True)]
          ]
 
-
-window = sg.Window('PikaChat', layout, default_element_size=(30, 2))
-
-isGroupChat = False
-outBox = window['OUT']
-
-consumerThread = threading.Thread(target=consume, kwargs={'outBox':outBox})
-publisherThread = threading.Thread(target=publish)
-
-consumerThread.start()
-#publisherThread.start()
-
-while True:
-    event, value = window.read()
-    if event in (sg.WINDOW_CLOSED, 'Exit'):
-        stopConsuming = True
-        break
-    #elif event == 'Send':
-        
-        """text += value['IN']+'\n'
-        
-        outBox.update(disabled = False)
-        outBox.update(text)
-        outBox.update(disabled = True)"""
-    elif event == 'Set':
-        queue_name = value['USER']
-
+# If user cancelled/closed last window, ends the program
+if close_program == False:
+    window = sg.Window('PikaChat', layout, default_element_size=(30, 2))
     
-    if value['R2'] == True:
-        isGroupChat = True
-        window['sec'].update(visible=isGroupChat)
-    else:
-        isGroupChat = False
-        window['sec'].update(visible=isGroupChat)
-
-window.close()
-
-"""consumerThread = threading.Thread(target=consume)
-publisherThread = threading.Thread(target=publish)
-
-consumerThread.start()
-publisherThread.start()"""
+    isGroupChat = False
+    outBox = window['OUT']
+    
+    consumerThread = threading.Thread(target=consume, kwargs={'outBox':outBox})
+    
+    connectionPublisher = pika.BlockingConnection(params)
+    channelPublisher = connectionPublisher.channel() # start a channel
+    channelPublisher.exchange_declare(exchange=exchange_name, exchange_type='fanout')
+    #publisherThread = threading.Thread(target=publish, kwargs={'window':window})
+    
+    consumerThread.start()
+    
+    while True:
+        event, value = window.read()
+        # Stops consuming if user closed the chat
+        if event in (sg.WINDOW_CLOSED, 'Exit'):
+            stopConsuming = True
+            break
+        # Send message
+        elif event == 'Send':
+            message = value['IN']+'\n'
+            
+            json_data["user"] = user_name
+            json_data["timestamp"] = str(datetime.datetime.now().replace(microsecond=0).isoformat())
+            json_data["message"] = message
+            
+            #channelPublisher.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(json_data))
+            channelPublisher.basic_publish(exchange=exchange_name, routing_key='', body=json.dumps(json_data))
+        # Direct message
+        elif event == 'Set':
+            queue_name = value['USER']
+    
+        if value['R2'] == True:
+            isGroupChat = True
+            window['sec'].update(visible=isGroupChat)
+        else:
+            isGroupChat = False
+            window['sec'].update(visible=isGroupChat)
+    
+    window.close()
